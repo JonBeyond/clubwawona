e8ddaf02b267481d94a9cfbc66795598bcc554de
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-03-15 15:28:19 -0700

    Allow user to navigate pages

--- frontend/src/modules/main/main.jsx
+++ frontend/src/modules/main/main.jsx
@@ -2,6 +2,12 @@
 // import axios from 'axios';
 import Navbar from './navbar/navbar.jsx';
 import Content from './content/content.jsx';
+
+//This container controls the state of all content
+//TODO: think about the best way to handle the RSVP fourm handler
+//TODO: especially consider about updating an RSVP
+//TODO: on the backend side, need a way to process the RSVP data
+//TODO: :D
 class Main extends React.Component{
   constructor() {
     super();
@@ -16,7 +22,7 @@ class Main extends React.Component{
       this.setState({
         page: event.target.className
       }, () => {
-        console.log(`navstate update to ${this.state.page}`);
+        console.log(`nNavstate update to ${this.state.page}`); //TODO: for dev only; remove
       })
     }
   }
