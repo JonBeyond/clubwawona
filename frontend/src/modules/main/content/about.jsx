e394582683f9ccfd96675818295ec89a62ab2be6
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-03-15 15:28:32 -0700

    Placeholder content pages

---
+++ frontend/src/modules/main/content/about.jsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      About Content
+      <div>Us</div>
+      <div>Soundsystem</div>
+      <div>Music</div>
+      <div>Entertainment</div>
+      <div>Drank</div>
+      <div>Safety</div>
+    </div>
+  );
+}
+
+export default About;
