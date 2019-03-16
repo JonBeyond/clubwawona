58b1379dbb899de225963da19c1227b2de1326cb
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-03-16 11:03:22 -0700

    Allowed for conditional content rendering

--- frontend/src/modules/main/content/content.jsx
+++ frontend/src/modules/main/content/content.jsx
@@ -1,16 +1,20 @@
 import About from './about.jsx';
+import Photos from './photos.jsx';
+import Residents from './Residents.jsx';
+import Contact from './Contact.jsx';
+import RSVP from './form.jsx';
 
 const Content = (props) => {
   if (props.page === 'RSVP') {
-    return (<div>TBD</div>);
+    return (<RSVP />);
   } else if (props.page === 'About') {
     return (<About />);
   } else if (props.page === 'Residents') {
-    return (<div>TBD</div>);
+    return (<Residents />);
   } else if (props.page === 'Photos') {
-    return (<div>TBD</div>);
+    return (<Photos />);
   } else if (props.page === 'Contact') {
-    return (<div>TBD</div>);
+    return (<Contact />);
   } else {
     return (
       <div>CONTENT RENDER ERROR</div>
