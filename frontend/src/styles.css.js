aa3b0c7126c09e86fc3898ac3b2813215481762c
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-03-16 11:53:44 -0700

    Updated styles

--- frontend/src/styles.css.js
+++ frontend/src/styles.css.js
@@ -1,8 +1,46 @@
 export default {
+
+  //********** HEADER **********
+
+  header: {
+    fontSize: '24px',
+    verticalAlign: 'middle',
+    textAlign: 'center'
+  },
+
+  //********** NAVBAR **********
+
+  navBar: {
+    verticalAlign: 'middle',
+    textAlign: 'center'
+  },
+
+  navBarElement: {
+    boxSizing: 'border-box',
+    display: 'inline-block',
+    listStylePosition: 'outside',
+    verticalAlign: 'top',
+    margin: '10px'
+  },
+
+  //********** CONTENT CONTAINER **********
+
+  main: {
+    verticalAlign: 'middle',
+    textAlign: 'center',
+    width: '600px',
+    display: 'block',
+    margin: 'auto'
+  },
+
+  //********** FORM **********
+
   form: {
     border: '3px solid white',
     width: '380px',
-    height: 'auto'
+    margin: 'auto',
+    height: 'auto',
+    verticalAlign: 'middle'
   },
 
   input: {
@@ -16,36 +54,12 @@ export default {
 
   button: {
     backgroundColor: 'purple',
-    color: 'green',
+    color: 'white',
+    fontWeight: '10',
     padding: '14px 20px',
     margin: '8px',
     border: 'none',
     cursor: 'pointer',
     width: '80%'
-  },
-
-  main: {
-    verticalAlign: 'right',
-    textAlign: 'center',
-    width: '600px'
-  },
-
-  header: {
-    fontSize: '24px',
-    verticalAlign: 'middle',
-    textAlign: 'center'
-  },
-
-  navBar: {
-    verticalAlign: 'middle',
-    textAlign: 'center'
-  },
-
-  navBarElement: {
-    boxSizing: 'border-box',
-    display: 'inline-block',
-    listStylePosition: 'outside',
-    verticalAlign: 'top',
-    margin: '10px'
   }
 }
