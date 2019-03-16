1dd8879270e4d206f74eac8ea8e32d8546eefd61
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-03-16 11:01:21 -0700

    Introduced basic styling

--- frontend/src/styles.css.js
+++ frontend/src/styles.css.js
@@ -1,6 +1,51 @@
 export default {
-  //example:
-  styleName: {
-    color: 'black'
+  form: {
+    border: '3px solid white',
+    width: '380px',
+    height: 'auto'
+  },
+
+  input: {
+    width: '95%',
+    padding: '12px 20px',
+    margin: '8px',
+    display: 'inline-block',
+    border: '1px solid white',
+    boxSizing: 'border-box'
+  },
+
+  button: {
+    backgroundColor: 'purple',
+    color: 'green',
+    padding: '14px 20px',
+    margin: '8px',
+    border: 'none',
+    cursor: 'pointer',
+    width: '80%'
+  },
+
+  main: {
+    verticalAlign: 'right',
+    textAlign: 'center',
+    width: '600px'
+  },
+
+  header: {
+    fontSize: '24px',
+    verticalAlign: 'middle',
+    textAlign: 'center'
+  },
+
+  navBar: {
+    verticalAlign: 'middle',
+    textAlign: 'center'
+  },
+
+  navBarElement: {
+    boxSizing: 'border-box',
+    display: 'inline-block',
+    listStylePosition: 'outside',
+    verticalAlign: 'top',
+    margin: '10px'
   }
 }
