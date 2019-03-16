3ea366a58596a6fd09dbfcd2fabe825dcc6fe40d
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-03-16 11:03:41 -0700

    Styled header

--- frontend/src/modules/header/header.jsx
+++ frontend/src/modules/header/header.jsx
@@ -1,6 +1,8 @@
+import styles from '../../styles.css.js';
+
 const Header = () => {
   return (
-    <div className='header'>
+    <div style={styles.header} className='header'>
       <div className='pagetitle'>Wawona</div>
       <div className='pagehook'>{randomHook()}</div>
     </div>
@@ -8,8 +10,9 @@ const Header = () => {
 }
 
 const randomHook = () => {
-  const hooks = ['Welcome to the party', 'This is how we do'];
-  return hooks[~~(Math.random()*hooks.length)];
+  // const hooks = ['Welcome to the party', 'This is how we do'];
+  // return hooks[~~(Math.random()*hooks.length)];
+  return '';
 }
 
 export default Header;
