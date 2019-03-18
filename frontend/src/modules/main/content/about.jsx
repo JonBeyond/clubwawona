b91867d12b94848320a63f1a263f3e13fab89b60
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-03-18 10:35:14 -0700

    Minor text updates

--- frontend/src/modules/main/content/about.jsx
+++ frontend/src/modules/main/content/about.jsx
@@ -2,13 +2,13 @@ const About = () => {
   return (
     <div>
       <div><b>About Us</b></div>
-      <p>We are a privately-owned and operated underground event venue in San Francisco, California, active since 2015.</p>
+      <p>We are a privately-owned and operated underground event venue in San Francisco, California, active since 2015.  With 4 dedicated rooms and over 500sq ft of lush dance floor, you won't want to party anywhere else.</p>
       <div><b>Soundsystem</b></div>
       <p>Club Wawona utilizes a custom, in-house made sound system, generating 500W of sound across quad-8" woofers, and dual 1.5" tweeters, utilizing a 4th order Linkwitz–Riley crossover to acheive incredible sound replication, backed by a 250W Dayton-powered, ground-rumbling 12" JL vented subwoofer - you'll never want to leave. </p>
+      <div><b>Atmosphere</b></div>
+      <p>Soar through the skies of possibility.  The only limit is your imagination.</p>
       <div><b>Music</b></div>
       <p>Please visit our residents page to learn more about the musical offerings.</p>
-      <div><b>Entertainment</b></div>
-      <p>Soar through the skies of possibility.  The only limit is your imagination.</p>
       <div><b>Membership</b></div>
       <p>Club Wawona is a private, invite-only venue.  There is no public membership.</p>
     </div>
