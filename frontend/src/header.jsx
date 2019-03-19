d0ef8f2012b8381c9a8eb5759547b11c66db9e28
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-03-19 12:29:11 -0700

    moved file

---
+++ frontend/src/header.jsx
@@ -0,0 +1,12 @@
+import styles from './styles.css.js';
+
+const Header = () => {
+  return (
+    <div style={styles.header} className='header'>
+      <div className='pagetitle'><img src='https://s3-us-west-1.amazonaws.com/clubwawona/title.png'></img></div>
+      <div className='pagehook'>Where the only limit is your imagination</div>
+    </div>
+  );
+}
+
+export default Header;
