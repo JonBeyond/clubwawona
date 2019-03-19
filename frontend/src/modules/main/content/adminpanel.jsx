96e04bb5cdacf208a6d6ff3608cdf07abd6056bc
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-03-19 11:46:15 -0700

    Create react element to render various admin panels

---
+++ frontend/src/modules/main/content/adminpanel.jsx
@@ -0,0 +1,17 @@
+import AdminReport from './adminreport.jsx';
+
+const AdminPanel = (props) => {
+  if (props.page === 'Report') {
+      return (<AdminReport />);
+  // } else if (props.page === 'Members') {
+  //     return (<AdminMembers />);
+  // } else if (props.page === 'Emails') {
+  //     return (<AdminEmails />);
+  } else {
+      return (
+      <div>CONTENT RENDER ERROR</div>
+      );
+  }
+}
+
+export default AdminPanel;
