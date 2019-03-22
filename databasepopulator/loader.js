889f2173053f2de4f79853f20433dfea5749b36e
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-03-22 14:59:44 -0700

    Created a database loading tool for client testing

--- databasepopulator/loader.js
+++ databasepopulator/loader.js
@@ -1,47 +1,126 @@
-// TODO:
-// 1) populate all 3 databases with hardcoded test params
-// 2) drop all databases
-//
-
-const fs = require('fs');
-const path = require('path');
 const mongoose = require('mongoose');
-const Master = require('../server/model.js').Master;
 const md5 = require('blueimp-md5');
+const Schema = require('../server/model.js');//can use: Master, RSVP, Credentials
+const config = require('../config.js');
 
-//Config files:
-const database = require('../config.js').database;
-const key = require('../config.js').tokenkey;
+//***********   GENERATE FAKE EMAILS   ***********//
+let emails = [];
+for (let i = 0; i < 50; i++) {emails.push(`fakeemail${i}@gmail.com`)}
 
-let emails = ['quiverinq@gmail.com'];
-//emails = fs.readFileSync(path.join(__dirname, 'emails.csv'),"utf8").toString().split(',')
 
-const update = (doc) => {
+//*********** REUSABLE HELPER FUNCTIONS ***********//
+//Upsert a record, via promise:
+//Example usage:
+  //query: {email: doc.email}
+  //cabinent: schema.RSVP, schema.Master, schema.Credentials
+
+const update = (doc, cabinent, query) => {
   return new Promise((resolve, reject) => {
-    Master.updateOne({email: doc.email}, doc, {upsert: true}, (err) => {
+    cabinent.updateOne(query, doc, {upsert: true}, (err) => {
       if(err) reject(err);
       resolve();
     });
+  });
+}
+
+//***********         LOADERS            ***********//
+const generateCredential = () => {
+  console.log(`creating MD5 with ${config.credential} and ${config.tokenkey}`);
+  let cred = {
+    password: md5(config.credential, config.tokenkey)
+  }
+  update(cred, Schema.Credentials, {password: cred.password})
+  .then(() => {
+    console.log('Database complete');
+    mongoose.connection.close();
+    process.exit();
+  })
+  .catch(err => {
+    console.log('Error creating credential');
+    mongoose.connection.close();
+    process.exit();
   })
 }
 
-mongoose.connect(database, { useNewUrlParser: true, useCreateIndex: true })
-  .then(() => {
-    let updates = [];
-    emails.forEach(email => {
-      let document = {
-        email: email,
-        token: md5(email,key),
-        sent: false
-      }
-      updates.push(update(document));
-    });
-    Promise.all(updates).then(() => {
-      mongoose.connection.close(); //just in case?? TODO: investigate.
-      process.exit();
-    })
-    .catch((err) => {
-      mongoose.connection.close(); //just in case?? TODO: investigate.
-      process.exit();
-    });
+const loadRSVPs = (documents) => {
+  let upserts = [];
+  documents.forEach(document => {
+    delete document['tokenSent'];
+    document['security'] = document['token'];
+    delete document['token']; //TODO:
+    document['guests'] = 0;
+    document['beer'] = 'Sours';
+    document['wine'] = 'Cab';
+    document['liquor'] = 'Tequila';
+    document['other'] = '';
+    upserts.push(update(document, Schema.RSVP, {email: document['email']}));
   });
+
+  Promise.all(upserts)
+  .then(() => {
+    console.log('RSVPs loaded');
+    generateCredential();
+  })
+  .catch((err) => {
+    console.log('error updating RSVPs');
+    console.log(err);
+    mongoose.connection.close();
+    process.exit();
+  })
+}
+
+const createMasterList = () => {
+  let upserts = [];
+  let docs = [];
+  emails.forEach(email => {
+    let document = {
+        email: email,
+        token: md5(email.toLowerCase(),config.tokenkey),
+        tokenSent: false,
+        firstName: 'blank',
+        lastName: 'blanky'
+    }
+    upserts.push(update(document, Schema.Master, {email: document.email}));
+    docs.push(document); //replace the email with the document.
+  });
+
+  Promise.all(upserts)
+  .then(() => {
+    console.log('Master list has been loaded.');
+    loadRSVPs(docs);
+  })
+  .catch((err) => {
+    console.log(`error updating master list: ${err}`);
+    mongoose.connection.close();
+    process.exit();
+  });
+}
+
+const dropDatabases = () => {
+  Schema.RSVP.deleteMany({}).then(() => {
+    console.log('RSVPs deleted.');
+    Schema.Master.deleteMany({}).then(() => {
+      console.log('Master list deleted.');
+      Schema.Credentials.deleteMany({}).then(() => {
+        console.log('Credentials deleted.');
+        createMasterList();
+      })
+    })
+  })
+  .catch(err => {
+    console.log('Error dropping databases');
+    mongoose.connection.close();
+    process.exit();
+  });
+}
+
+//****** LOADER / INITIALIZER ******//
+(function () {
+  mongoose.connect(config.database, { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => dropDatabases())
+  .catch((err) => {
+    console.log('error connecting to the database');
+    mongoose.connection.close();
+    process.exit();
+  });
+})()
