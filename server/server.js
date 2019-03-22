3499c41c23731cfd4bb744a2c0e585cbc147fd98
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-03-22 15:00:43 -0700

    Refactored code to increase clarity and remove unncessary lines

--- server/server.js
+++ server/server.js
@@ -11,26 +11,13 @@ server.use(bodyParser.json());
 //once deployed, bundle should be served from S3
 server.use('/',express.static(path.join(__dirname, '../frontend/dist')));
 
-server.post('/api/RSVP', (req, res) => {
-  //TODO: check for proper headers / key
-  controller.process.RSVP(req.body, res);
-})
+//TODO: check for proper headers / key
+server.post('/api/RSVP', (req, res) => controller.process.RSVP(req.body, res));
+server.get('/api/members', (req, res) => controller.process.allMembers(res));
+server.post('/api/authenticate', (req, res) => controller.process.login(req.body.credential, res));
+server.get('/api/report', (req, res) => controller.process.allResponses(res))
 
-server.get('/api/members', (req, res) => {
-  controller.process.allMembers(res);
-})
-
-server.post('/api/authenticate', (req, res) => {
-  controller.process.login(req.body, res);
-})
-
-server.get('/api/report', (req, res) => {
-  controller.process.allResponses(res);
-})
-
-server.listen(port, () => {
-  console.log(`Server is listening on ${port}`);
-})
+server.listen(port, () => console.log(`Server is listening on ${port}`))
 
 //In case of unknown error, we don't want to shut down the server:
 //--> this would be bad practice in a profesional server, but it is OK
