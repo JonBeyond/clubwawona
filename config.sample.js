6a5f32698096d03a23418f698d0e15444557113f
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-03-22 11:15:46 -0700

    Updated sample config

--- config.sample.js
+++ config.sample.js
@@ -1,3 +1,6 @@
 module.exports = {
-  database: 'mongodb://user:pass@mongourl'
+  database: 'mongodb://user:pass@mongourl', //mongodb location
+  tokenkey: '', //string representing MD5 hash key
+  apikey: '', //is this used?
+  credential: '' //temporary, for saving the password as MD5
 }
