28db78c354916fb3cf52324af061005cfb680656
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-03-22 12:00:08 -0700

    Added authentication check

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,3 +1,18 @@
 const md5 = require('blueimp-md5');
 const key = require('../config.js').tokenkey;
+const lookup = require('../config.js').lookup;
+const Credentials = require('../model.js').Master;
 
+const login = (credential, res) => {
+  let hashCredential = md5(credential,key);
+  Credentials.findOne({lookup: lookup}, (err, document) => {
+    if (err) res.send(500);
+    else if (document && document.password === hashCredential) {
+      res.send('PASSED');
+    } else {
+      res.send('badkey');
+    }
+  });
+}
+
+module.exports = { login };
