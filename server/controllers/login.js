1fe7189fd4cc03f2c1473568b18fdf2a20b9d296
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-03-23 16:06:27 -0700

    Now using bcrypt for authentication, and generating a temporary API key for use by the admin

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,13 +1,27 @@
 const bcrypt = require('bcrypt');
 const Credentials = require('../model.js').Credentials;
+var authKey = null;
 
 const login = (providedPassword, res) => {
   Credentials.findOne({type: 'admin'})
   .then(credential => {
     bcrypt.compare(providedPassword,  credential.token)
     .then(response => {
-      if(response) res.send('PASSED')
-      else res.send('badkey')
+      if(response) {
+        //generate and store a key that will be deleted after a set time
+        let response = {
+          status: 'PASSED',
+          APIKey: generateTemporaryKey()
+        }
+        res.send(JSON.stringify(response));
+      }
+      else {
+        let response = {
+          status: 'badkey',
+          APIKey: null
+        }
+        res.send(JSON.stringify(response));
+      }
     })
   })
   .catch(err => {
@@ -16,4 +30,22 @@ const login = (providedPassword, res) => {
   });
 }
 
-module.exports = { login };
+const generateTemporaryKey = () => {
+  authKey = ~~(Math.random()*100000000);
+  console.log(`Generated new key: ${authKey}`);
+  //generate a temporary key (Only one session can be active at any time)
+  setTimeout(() => {
+    console.log(`Deleting authkey: ${authKey}`);
+    authKey = null;
+    console.log(`Authkey should be null: ${authKey}`);
+  }, 600000);
+  return authKey;
+}
+
+const checkCurrentAuthKey = (key) => {
+  console.log(`comparing providied: ${key} with official: ${authKey}`);
+  if (key === null) return false;
+  else return key === authKey;
+}
+
+module.exports = { login, checkCurrentAuthKey };
