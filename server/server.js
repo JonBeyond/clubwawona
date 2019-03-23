49843df92eb5473609f247ce1c6dbabad807729b
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-03-23 16:38:34 -0700

    Hid entry point behind config

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const server = express();
 const bodyParser = require("body-parser");
 const path = require('path');
 const controller = require('./controller.js');
+const endpoint = require('../config.js').endpoint;
 const port = 3000;
 
 server.use(bodyParser.json());
@@ -14,7 +15,7 @@ server.use('/',express.static(path.join(__dirname, '../frontend/dist')));
 server.post('/api/RSVP', (req, res) => controller.process.RSVP(req.body, res));
 server.get('/api/members/:auth', (req, res) => controller.process.allMembers(req, res));
 server.get('/api/report/:auth', (req, res) => controller.process.allResponses(req, res));
-server.post('/api/authenticate', (req, res) => controller.process.login(req.body.credential, res));
+server.post(`/api/authenticate/${endpoint}`, (req, res) => controller.process.login(req.body.credential, res));
 
 server.listen(port, () => console.log(`Server is listening on ${port}`))
 
