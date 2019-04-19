30cb1e1628d304ead43cbc04c4f9c0ae06429cd6
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-04-19 12:48:31 -0700

    Added stub code for new API endpoints and email functionality

--- frontend/src/modules/admin/admin.jsx
+++ frontend/src/modules/admin/admin.jsx
@@ -94,17 +94,34 @@ class Admin extends React.Component {
     }
   }
 
-  sendEmail(email) {
+  sendEmail(event) { //TODO:
     //Process: verify if email was sent
     // -> if yes, then don't sent (it must be reset to make sure emails don't get sent more than once);
     //
+    //this needs to send an API request with the data.
+    //since the client has the data, should we just send it?
+    let email = event.target.id;
+
     console.log('this feature is not yet available');
+
+    //process:
+    //1) create a new API endpoint for sending and resetting email state TODO:
+    //2) send the email over API
+    //3) the server will interact with OAUTH/etc.
+    //4) there needs to be new router and controller code on the server side
+    // For now, this section just needs to make an API call and handle errors.
+
   }
 
-  resetEmail(email) {
+  resetEmail(event) { //TODO:
     //RESET the email boolean to allow the button to send.
     //perhaps there should be a confirmation popup?
     //need to weigh spamming people
+    //TODO: this is similar to the above, only needs to make an API call and handle
+    // errors.  Any updates should trigger a state refresh. TODO: think about this a little more.
+    // Do I want a full data refresh for every minor change?
+    let email = event.target.id;
+
     console.log('this feature is not yet available');
   }
 
