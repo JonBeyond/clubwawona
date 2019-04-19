6cee888cbf86cf97e92bbf91e4cc747e5ef74b66
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-04-19 12:47:06 -0700

    Minor member cleanup

--- frontend/src/modules/admin/member.jsx
+++ frontend/src/modules/admin/member.jsx
@@ -1,41 +1,19 @@
-const style = { //keep this style here, because it's an admin style? TODO: move to styles.css.js
-  textAlign: 'center',
-  padding: '10px',
-  width: '600px',
-  border: '1px solid white',
-  boxSizing: 'border-box',
-  display: 'inline-block'
-
-}
-
 const Member = (props) => {
-  // console.log(props.member);
-  return (<div>
-    <table style={style}>
-    <thead>
-      <tr>
-        <td>Name</td>
-        <td>Email</td>
-        <td>Email Sent?</td>
-      </tr>
-    </thead>
+  return (
     <tbody>
       <tr>
         <td>{props.member['name']}</td>
         <td>{JSON.stringify(props.member['tokenSent'])}</td>
         <td>{props.member['email']}</td>
-        <td><button onClick={props.sendEmail} >Send Email</button></td>
-        <td><button onClick={props.resetEmail} >Reset Email</button></td>
+        <td><button id={props.member['email']} onClick={props.sendEmail} >Send Email</button></td>
+        <td><button id={props.member['email']} onClick={props.resetEmail} >Reset Email</button></td>
       </tr>
       <tr>
-        <td colSpan='5'>{props.member['token']}</td>
+        <td colSpan='5'>TOKEN: {props.member['token']}</td>
       </tr>
-    </tbody>
-    </table>
-  </div>)
+      <tr>
+        <td colSpan='5'>————————————————————</td>
+      </tr>
+    </tbody>);
 }
-
-
-
-
 export default Member;
