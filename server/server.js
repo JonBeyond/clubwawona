d163949840e432e6a3b1a769743795dfdc56b9f4
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-04-24 14:48:16 -0700

    Rearranged file and added 3 new endpoints for master list CRUD

--- server/server.js
+++ server/server.js
@@ -12,14 +12,26 @@ server.use(bodyParser.json());
 //once deployed, bundle should be served from S3
 server.use('/',express.static(path.join(__dirname, '../frontend/dist')));
 
-server.post('/api/RSVP', (req, res) => controller.process.RSVP(req.body, res));
-server.get('/api/members/:auth', (req, res) => controller.process.allMembers(req, res));
-server.get('/api/report/:auth', (req, res) => controller.process.allResponses(req, res));
-server.post(`/api/authenticate/${endpoint}`, (req, res) => controller.process.login(req.body.credential, res));
+//Client RSVP:
+server.post('/api/RSVP', (req, res) => controller.RSVP(req, res));
+
+//Admin Login:
+server.post(`/api/authenticate/${endpoint}`, (req, res) => controller.login(req, res)); //TODO: this endpoint may not be following the RESTful standards
+
+//Admin Metrics:
+server.get('/api/members/:auth', (req, res) => controller.allMembers(req, res)); //TODO: this endpoint may not be following the RESTful standards
+server.get('/api/report/:auth', (req, res) => controller.allResponses(req, res));
+
+//Admin Management:
+server.post('/api/master/:auth', (req, res) => controller.addMember(req, res));
+server.patch('/api/master/:auth/reset', (req, res) => controller.resetEmail(req, res));
+server.delete('/api/master/:auth/:email', (req, res) => controller.deleteMember(req, res));
+
+//TODO: endpoints for sending emails
 
 server.listen(port, () => console.log(`Server is listening on ${port}`))
 
-//In case of unknown error, we don't want to shut down the server:
+//In case of unknown error, I don't want to shut down the server:
 //--> this would be bad practice in a profesional server, but it is OK
 //for this private website.
 process.on('uncaughtException', function (err) {
