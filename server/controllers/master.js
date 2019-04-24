e7be651bdc5ca6f6df88dae40b6bbc6bc12a6d8e
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-04-24 14:46:22 -0700

    Created module for master list CRUD operations; finished delete and patch functionality

---
+++ server/controllers/master.js
@@ -0,0 +1,50 @@
+const Master = require('../model.js').Master;
+const checkCurrentAuthKey = require('./authorization.js').checkCurrentAuthKey;
+
+const resetEmail = (req, res) => {
+  if (checkCurrentAuthKey(req.params.auth)) {
+    Master.findOneAndUpdate({email: req.body.email.toLowerCase()}, {tokenSent: false}, {upsert: false}, (err, doc)  => {
+      if (err) {
+        res.sendStatus(500);
+        console.error(`Error reseting email: ${err}`);
+      } else if (doc) {
+        res.sendStatus(200);
+      } else {
+        console.error(`Attempted reset to of a non-existant email: ${req.body.email.toLowerCase()}`);
+        res.sendStatus(500);
+      }
+    });
+  } else {
+    res.sendStatus(401);
+  }
+}
+
+const addMember = (req, res) => {
+  //TODO: add a new email into the database
+  //use 'upsert'.  This will allow for name changes.
+  //handle errors
+};
+
+const deleteMember = (req, res) => {
+  if (checkCurrentAuthKey(req.params.auth)) {
+    console.log(`Looking for ${req.params.email} and removing`);
+    Master.findOneAndDelete({email: req.params.email})
+    .then((doc) => {
+      if (doc) {
+        console.log(`${JSON.stringify(doc)} removed from list`);
+        res.sendStatus(200);
+      } else {
+        console.log(`Email not found`);
+        res.sendStatus(404);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    })
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = { resetEmail, addMember, deleteMember };
